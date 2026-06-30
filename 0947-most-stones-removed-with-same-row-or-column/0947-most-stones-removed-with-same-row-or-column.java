3b0d7c169d1944b3417525c2fc98624d72043be4
Avtar Kumar <avtarkumar8483@gmail.com>
2026-07-01 02:23:53 +0530

    Time: 14 ms (74.13%), Space: 47.2 MB (10.87%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,35 +1,27 @@
-class DSU {
-    Map<Integer, Integer> parent = new HashMap<>();
-
-    public int find(int x) {
-        parent.putIfAbsent(x, x);
-
-        if (parent.get(x) != x) {
-            parent.put(x, find(parent.get(x)));
-        }
-
-        return parent.get(x);
-    }
-
-    public void union(int x, int y) {
-        parent.put(find(x), find(y));
-    }
-}
-
 class Solution {
-    public int removeStones(int[][] stones) {
-        DSU dsu = new DSU();
+    Map<Integer,Integer> parent = new HashMap<>();
 
-        for (int[] stone : stones) {
-            dsu.union(stone[0], stone[1] + 10001);
+    public int find(int x){
+        parent.putIfAbsent(x,x);
+        if(parent.get(x) != x){
+            parent.put(x,find(parent.get(x)));
+        }
+        return parent.get(x);
+    } 
+
+    public void union(int x, int y){
+        parent.put(find(x),find(y));
+    }
+
+    public int removeStones(int[][] stones) {
+        for(int[] stone : stones){
+            union(stone[0],stone[1] + 10001);
         }
 
         HashSet<Integer> components = new HashSet<>();
-
-        for (int[] stone : stones) {
-            components.add(dsu.find(stone[0]));
+        for(int[] stone : stones){
+            components.add(find(stone[0]));
         }
-
         return stones.length - components.size();
     }
 }
