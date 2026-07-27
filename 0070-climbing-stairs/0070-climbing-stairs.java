b0a91b384332055bb78229d556a86d3c1e208ab4
Avtar Kumar <avtarkumar8483@gmail.com>
2026-07-27 20:02:53 +0530

    Time: 0 ms (100%), Space: 42 MB (73.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution { ////     Introduction to [ DP ]
     int[] dp;
     int climb(int n){
         if(n==1){
@@ -18,3 +18,52 @@ class Solution {
         return climb(n);
     }
 }
+
+// tabulation form
+
+// class Solution {
+
+//     public int climbStairs(int n) {
+
+//         if(n<=2)
+//             return n;
+
+//         int dp[]=new int[n+1];
+
+//         dp[1]=1;
+//         dp[2]=2;
+
+//         for(int i=3;i<=n;i++){
+
+//             dp[i]=dp[i-1]+dp[i-2];
+
+//         }
+
+//         return dp[n];
+//     }
+// }
+
+
+//Space optimized 
+
+// class Solution {
+
+//     public int climbStairs(int n) {
+
+//         if(n<=2)
+//             return n;
+
+//         int prev2 = 1;
+//         int prev1 = 2;
+
+//         for(int i=3;i<=n;i++){
+
+//             int curr = prev1 + prev2;
+
+//             prev2 = prev1;
+//             prev1 = curr;
+//         }
+
+//         return prev1;
+//     }
+// }
