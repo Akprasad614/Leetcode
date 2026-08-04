b2c0cb9782b03b895f765e6804613f20df44e930
Avtar Kumar <avtarkumar8483@gmail.com>
2026-08-04 14:16:57 +0530

    Time: 1 ms (14.46%), Space: 42.3 MB (13.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,48 @@
 class Solution {
     int[][] dp;
     int countPath(int m,int n){
-        if(m < 0 || n < 0){
-            return 0;
+        dp[0][0] = 1;
+        for(int i =0;i<m; i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                int up = 0;
+                int left = 0;
+
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                dp[i][j] = left + up;
+            }
         }
-        if(m == 0 && n == 0){
-            return 1;
-        }
-        if(dp[m][n] != -1){
-            return dp[m][n];
-        }
-        int up = countPath(m-1,n);
-        int left = countPath(m,n-1);
-        return dp[m][n] = left + up;
+        return dp[m-1][n-1];
     }
     public int uniquePaths(int m, int n) {
         dp = new int[m][n];
-        for(int[] rows : dp){
-           Arrays.fill(rows,-1);
-        }
-        return countPath(m-1,n-1);
+        return countPath(m,n);
     }
 }
+
+
+// class Solution {            // DP 
+//     int[][] dp;
+//     int countPath(int m,int n){
+//         if(m < 0 || n < 0){
+//             return 0;
+//         }
+//         if(m == 0 && n == 0){
+//             return 1;
+//         }
+//         if(dp[m][n] != -1){
+//             return dp[m][n];
+//         }
+//         int up = countPath(m-1,n);
+//         int left = countPath(m,n-1);
+//         return dp[m][n] = left + up;
+//     }
+//     public int uniquePaths(int m, int n) {
+//         dp = new int[m][n];
+//         for(int[] rows : dp){
+//            Arrays.fill(rows,-1);
+//         }
+//         return countPath(m-1,n-1);
+//     }
+// }
