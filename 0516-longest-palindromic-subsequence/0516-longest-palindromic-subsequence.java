72a0b79ca27e900238dbc8e536eec3c6929a35b8
Avtar Kumar <avtarkumar8483@gmail.com>
2026-08-24 23:39:05 +0530

    Time: 71 ms (5.44%), Space: 46.5 MB (96.47%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,23 +1,54 @@
 class Solution {
-    int dp[][];
+
     public int longestPalindromeSubseq(String s) {
+
         int n = s.length();
         String rev = new StringBuilder(s).reverse().toString();
-        dp = new int[n+1][n+1];
-        for(int i =1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(s.charAt(i-1) == rev.charAt(j-1)){
-                dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+
+        int[] prev = new int[n + 1];
+        int[] curr = new int[n + 1];
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = 1; j <= n; j++) {
+
+                if(s.charAt(i - 1) == rev.charAt(j - 1)) {
+
+                    curr[j] = 1 + prev[j - 1];
+
+                } else {
+
+                    curr[j] = Math.max(prev[j], curr[j - 1]);
                 }
             }
-        }
-        return dp[n][n];
 
+            prev = curr;
+            curr = new int[n + 1];
+        }
+
+        return prev[n];
     }
 }
+// class Solution {
+//     int dp[][];
+//     public int longestPalindromeSubseq(String s) {
+//         int n = s.length();
+//         String rev = new StringBuilder(s).reverse().toString();
+//         dp = new int[n+1][n+1];
+//         for(int i =1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 if(s.charAt(i-1) == rev.charAt(j-1)){
+//                 dp[i][j] = 1 + dp[i-1][j-1];
+//                 }
+//                 else{
+//                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+//                 }
+//             }
+//         }
+//         return dp[n][n];
+
+//     }
+// }
 
 // class Solution {
 //     int dp[][];
