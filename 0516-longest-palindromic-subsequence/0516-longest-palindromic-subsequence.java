5db1faa949a5b44348d0700401a4cf7ccc1007ff
Avtar Kumar <avtarkumar8483@gmail.com>
2026-08-24 23:35:57 +0530

    Time: 60 ms (24.87%), Space: 63.5 MB (70.35%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,24 +1,45 @@
 class Solution {
     int dp[][];
-    int lcs(int i,int j,String s,String rev){
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(s.charAt(i) == rev.charAt(j)){
-            return dp[i][j] = 1 + lcs(i-1,j-1,s,rev);
-        }
-        return dp[i][j] = Math.max(lcs(i-1,j,s,rev),lcs(i,j-1,s,rev));
-    }
     public int longestPalindromeSubseq(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
         int n = s.length();
-        dp = new int[n][n];
-        for(int[] rows:dp){
-            Arrays.fill(rows,-1);
+        String rev = new StringBuilder(s).reverse().toString();
+        dp = new int[n+1][n+1];
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1) == rev.charAt(j-1)){
+                dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
-        return lcs(n-1,n-1,s,rev);
+        return dp[n][n];
+
     }
 }
+
+// class Solution {
+//     int dp[][];
+//     int lcs(int i,int j,String s,String rev){
+//         if(i<0 || j<0){
+//             return 0;
+//         }
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+//         if(s.charAt(i) == rev.charAt(j)){
+//             return dp[i][j] = 1 + lcs(i-1,j-1,s,rev);
+//         }
+//         return dp[i][j] = Math.max(lcs(i-1,j,s,rev),lcs(i,j-1,s,rev));
+//     }
+//     public int longestPalindromeSubseq(String s) {
+//         String rev = new StringBuilder(s).reverse().toString();
+//         int n = s.length();
+//         dp = new int[n][n];
+//         for(int[] rows:dp){
+//             Arrays.fill(rows,-1);
+//         }
+//         return lcs(n-1,n-1,s,rev);
+//     }
+// }
