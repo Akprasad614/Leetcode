43070b8714d8451954eabb2ba0419e8c41f5cf45
Avtar Kumar <avtarkumar8483@gmail.com>
2026-08-27 23:31:26 +0530

    Time: 16 ms (83.63%), Space: 46.6 MB (85.93%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,29 +1,52 @@
 class Solution {
-    int [][]dp;
-    int distinct(int i,int j,String s,String t){
-        if(j<0){
-            return 1;
-        }
-        if(i<0){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = distinct(i-1,j-1,s,t) + distinct(i-1,j,s,t);
-        }
-        else{
-            return dp[i][j] = distinct(i-1,j,s,t);
-        }
-    }
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
-        dp = new int[n][m];
-        for(int rows[] : dp){
-            Arrays.fill(rows,-1);
+        int[] prev = new int[m+1];
+        prev[0] = 1;
+        for(int i=1;i<=n;i++){
+            int[] temp = new int[m+1];
+            temp[0] = 1;
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    temp[j] = prev[j-1] + prev[j];
+                }
+                else{
+                    temp[j] = prev[j];
+                }
+            }
+            prev = temp;
         }
-        return distinct(n-1,m-1,s,t);
+        return prev[m];
     }
 }
+
+// class Solution {
+//     int [][]dp;
+//     int distinct(int i,int j,String s,String t){
+//         if(j<0){
+//             return 1;
+//         }
+//         if(i<0){
+//             return 0;
+//         }
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+//         if(s.charAt(i) == t.charAt(j)){
+//             return dp[i][j] = distinct(i-1,j-1,s,t) + distinct(i-1,j,s,t);
+//         }
+//         else{
+//             return dp[i][j] = distinct(i-1,j,s,t);
+//         }
+//     }
+//     public int numDistinct(String s, String t) {
+//         int n = s.length();
+//         int m = t.length();
+//         dp = new int[n][m];
+//         for(int rows[] : dp){
+//             Arrays.fill(rows,-1);
+//         }
+//         return distinct(n-1,m-1,s,t);
+//     }
+// }
