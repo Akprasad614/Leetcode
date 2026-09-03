bc50e3d270d1bb629c6fe1d5a612e319d3c53831
Avtar Kumar <avtarkumar8483@gmail.com>
2026-09-03 18:48:21 +0530

    Time: 0 ms (100%), Space: 46.2 MB (92.6%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,25 +1,40 @@
-class Solution {
-    int[][] dp;
-    int profit(int i,int status,int[] prices){
-        if(i == prices.length){
-            return 0;
-        }
-        if(dp[status][i] != -1){
-            return dp[status][i];
-        }
-        if(status == 0){
-            return dp[0][i] = Math.max(-prices[i] + profit(i+1,1,prices),0 + profit(i+1,0,prices));
-        }
-        else{
-            return dp[1][i] = Math.max(prices[i] + profit(i+1,0,prices),0 + profit(i+1,1,prices));
-        }
-    }
+//     GREEDY
+class Solution {         
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        dp = new int[2][n];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        int profit=0;
+        for(int i=1;i<prices.length;i++) {
+            if(prices[i]>prices[i-1]) {
+                profit+=prices[i]-prices[i-1];
+            }
         }
-        return profit(0,0,prices);
+        return profit;
     }
 }
+
+//        DP
+
+// class Solution {
+//     int[][] dp;
+//     int profit(int i,int status,int[] prices){
+//         if(i == prices.length){
+//             return 0;
+//         }
+//         if(dp[status][i] != -1){
+//             return dp[status][i];
+//         }
+//         if(status == 0){
+//             return dp[0][i] = Math.max(-prices[i] + profit(i+1,1,prices),0 + profit(i+1,0,prices));
+//         }
+//         else{
+//             return dp[1][i] = Math.max(prices[i] + profit(i+1,0,prices),0 + profit(i+1,1,prices));
+//         }
+//     }
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         dp = new int[2][n];
+//         for(int[] row : dp){
+//             Arrays.fill(row,-1);
+//         }
+//         return profit(0,0,prices);
+//     }
+// }
